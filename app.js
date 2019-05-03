3a22c2f395ca71f8c74dbbf1312be57b64bd2adb
unknown <mubakhankhalil@gmail.com>
2019-05-03 16:00:41 +0500

    hi me

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.post("/get-items", async (req, res) => {
 const getItems = async url => {
     var arrItems = [];
 
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: true ,args: ['--no-sandbox']});
     const page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 926 });
     await page.goto(url);
